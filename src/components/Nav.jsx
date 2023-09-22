5575a37d0298e9969fadee6063a7cde42f35402f
YOUR NAME <ritter.christina.r.c@gmail.com>
2023-09-22 11:03:04 +0200

    :compass: navbar finished

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,35 @@
+import { headerLogo } from '../assets/images';
+import { hamburger } from '../assets/icons';
+import { navLinks } from '../constants';
+
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <header className="padding-x py-8 absolute-10 w-full">
+      <nav className="flex justify-between items-center max-container">
+        <a href="/">
+          <img src={headerLogo} alt="Logo" width={130} height={29} />
+        </a>
+        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <a className="font-montserrat leading-normal text-lg text-slate-gray">
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div>
+          <img
+            src={hamburger}
+            alt="hamburger"
+            width={20}
+            height={20}
+            className="hidden max-lg:block"
+          />
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Nav;
